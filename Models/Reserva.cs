a3c722d12149ac6f8e9a68006ea917acba4bda60
daniel-q <daniel.q.aquino@gmail.com>
2025-10-01 02:14:37 +0000

    feat: Classe Reserva finalizada

--- Models/Reserva.cs
+++ Models/Reserva.cs
@@ -15,16 +15,14 @@ namespace DesafioProjetoHospedagem.Models
 
         public void CadastrarHospedes(List<Pessoa> hospedes)
         {
-            // TODO: Verificar se a capacidade é maior ou igual ao número de hóspedes sendo recebido
-            // *IMPLEMENTE AQUI*
-            if (true)
+            bool podeHospedar = Suite is not null && hospedes.Count <= Suite.Capacidade;
+            if (podeHospedar)
             {
                 Hospedes = hospedes;
             }
             else
             {
-                // TODO: Retornar uma exception caso a capacidade seja menor que o número de hóspedes recebido
-                // *IMPLEMENTE AQUI*
+                throw new Exception("A quantidade de hospedes não pode exeder a capacidade da suite");
             }
         }
 
@@ -35,25 +33,15 @@ namespace DesafioProjetoHospedagem.Models
 
         public int ObterQuantidadeHospedes()
         {
-            // TODO: Retorna a quantidade de hóspedes (propriedade Hospedes)
-            // *IMPLEMENTE AQUI*
-            return 0;
+            
+            return Hospedes is not null? Hospedes.Count:0;
         }
 
         public decimal CalcularValorDiaria()
         {
-            // TODO: Retorna o valor da diária
-            // Cálculo: DiasReservados X Suite.ValorDiaria
-            // *IMPLEMENTE AQUI*
-            decimal valor = 0;
 
-            // Regra: Caso os dias reservados forem maior ou igual a 10, conceder um desconto de 10%
-            // *IMPLEMENTE AQUI*
-            if (true)
-            {
-                valor = 0;
-            }
-
+            decimal valor = DiasReservados<10 ? DiasReservados*Suite.ValorDiaria*1.00M:
+                                                DiasReservados *Suite.ValorDiaria*0.90M;
             return valor;
         }
     }
